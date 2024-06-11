408c4763fa6d0f6596c403f1863f42df866a21d9
Martim <martimfernandes2005@gmail.com>
2024-06-11 14:59:13 +0100

    php do login

---
+++ scripts/criarconta.php
@@ -0,0 +1,9 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nome = $_POST["nome"];
+    $cc = $_POST["cc1"];
+    $email = $_POST["email1"];
+    $senha = $_POST["senha1"];
+    $userType = $_POST["user"];
+}
+?>
